d221099519059ac567ee5ba22541350a13346e0e
shakhawat <shakhawat.sazu@gmail.com>
2021-03-12 20:46:30 +0600

    message

---
+++ app/src/main/java/com/example/sing_in/Homepage.java
@@ -0,0 +1,16 @@
+package com.example.sing_in;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
--- app/src/main/java/com/example/sing_in/MainActivity.java
+++ app/src/main/java/com/example/sing_in/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.sing_in;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login,singUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    login= findViewById(R.id.blnSingin);
+    singUp= findViewById(R.id.btnSingup);
+    login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent gotoHomepage= new Intent(MainActivity.this,Homepage.class);
+            startActivity(gotoHomepage);
+
+        }
+    });
+
+
+    singUp.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent gotoRegistration= new Intent(MainActivity.this,Registration.class);
+            startActivity(gotoRegistration);
+        }
+    });
+
     }
+
 }
