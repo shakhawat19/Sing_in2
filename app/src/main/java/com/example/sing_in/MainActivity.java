982f924b425bd63cf1f197c149250df1bde513bf
shakhawat <shakhawat.sazu@gmail.com>
2021-03-12 21:24:31 +0600

    message

--- app/src/main/java/com/example/sing_in/MainActivity.java
+++ app/src/main/java/com/example/sing_in/MainActivity.java
@@ -3,19 +3,29 @@ package com.example.sing_in;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login,singUp;
+    Button login,singUp,btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     login= findViewById(R.id.blnSingin);
+    btn1=findViewById(R.id.btn1);
+    btn1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+           Intent i= new Intent(Intent.ACTION_VIEW);
+            Uri.parse("https://fonts.google.com/");
+            startActivity(i);
+        }
+    });
     singUp= findViewById(R.id.btnSingup);
     login.setOnClickListener(new View.OnClickListener() {
         @Override
